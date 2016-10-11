c6e87dcfd079fac68945521ab8553c14b0b9b033
范捷琦 <jieqi.fan@corp.elong.com>
2016-10-11 16:34:53 +0800

    remove deprecated rspec:config
    under the rspec_v3.5.3
    
    RSpec::Core::Configuration#treat_symbols_as_metadata_keys_with_true_valu
    es= is deprecated, it is now set to true as default and setting it to
    false has no effect.
    
    undefined method `color_enabled=‘, change it to config.color=

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require "seventeen_mon"
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
   config.order = 'random'
 
-  config.color_enabled = true
+  config.color = true
 end
